d00311f63b6c9e1769bbcd6f0909d072bb591a52
Yasmin Vizeu <101536772+yvenglish@users.noreply.github.com>
2026-06-01 12:08:45 -0300

    Update story dates in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const stories = [
   {
-    date: "July, 1, 2026",
+    date: "July 1, 2026",
     title: "Israel Expands Offensive in Lebanon",
 
     levels: {
@@ -37,7 +37,7 @@ const stories = [
   },
 
   {
-    date: "May 26, 2026",
+    date: "July 2, 2026",
     title: "High Gas Prices Push Inflation Higher",
 
     levels: {
@@ -74,7 +74,7 @@ const stories = [
   },
 
   {
-    date: "May 27, 2026",
+    date: "July 3, 2026",
     title: "Experts Warn About Quantum Cybersecurity Risk",
 
     levels: {
